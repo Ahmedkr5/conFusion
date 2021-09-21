112eff783687b0db9be0a269eb33f34bb53256d2
Ahmedkr5 <ahmed.khiari2@esprit.tn>
2021-09-21 13:15:47 +0100

    Express Generator REST API

---
+++ routes/promoRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+
+promoRouter.route('/') // path spécifique
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotions to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions');
+});
+
+promoRouter.get('/:promoId', (req,res,next) => {
+    res.end('Will send details of the promotion: ' + req.params.promoId +' to you!');
+  });
+  
+  promoRouter.post('/:promoId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotions/'+ req.params.promoId);
+  });
+  
+  promoRouter.put('/:promoId', (req, res, next) => {
+    res.write('Updating the promotion: ' + req.params.promoId + '\n');
+    res.end('Will update the promotion: ' + req.body.name + 
+          ' with details: ' + req.body.description);
+  });
+
+  promoRouter.delete('/:promoId', (req, res, next) => {
+      res.end('Deleting promotion: ' + req.params.promoId);
+  });
+
+
+module.exports = promoRouter;
